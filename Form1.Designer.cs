63752da98cddb9b856e25e975f91bb592eabaa47
Vasily Vinogradov <73548481+Thamgr@users.noreply.github.com>
2022-11-21 00:46:44 +0300

    +commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1596, 1018);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.YellowGreen;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1596, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go Mandelbrot!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1596, 1050);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        float dx1, dy1, dx2, dy2;
+        float rx1, ry1, rx2, ry2;
+        float rrx1, rry1;
+        int flag = 0;
+        int iter = 300;
+        List<int> cred = new List<int>();
+        List<int> cgreen = new List<int>();
+        List<int> cblue = new List<int>();
+        Random rnd = new Random();
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine($"> {e.X} {e.Y}");
+            float nx = e.X, ny = e.Y;
+            if (flag == 0)
+            {
+                rrx1 = (rx2 - rx1) * nx / (dx2 - dx1) + rx1;
+                rry1 = (ry2 - ry1) * ny / (dy2 - dy1) + ry1;
+            }
+            else
+            {
+                rx2 = (rx2 - rx1) * nx / (dx2 - dx1) + rx1;
+                ry2 = rry1 + (rx2 - rrx1) * dy2 / dx2;
+                rx1 = rrx1;
+                ry1 = rry1;
+            }
+            flag = (flag + 1) % 2;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dx1 = 0;
+            dy1 = 0;
+            dx2 = this.pictureBox1.Width;
+            dy2 = this.pictureBox1.Height;
+            rx1 = -4f;// -this.pictureBox1.Width / 2;
+            ry1 = -2f;//-this.pictureBox1.Height / 2;
+            rx2 = 4f;// this.pictureBox1.Width / 2;
+            ry2 = 2f;// this.pictureBox1.Height / 2;
+            for (int i = 0; i < iter; ++i)
+            {
+                cred.Add(rnd.Next(128, 256));
+                cgreen.Add(rnd.Next(128, 256));
+                cblue.Add(rnd.Next(128, 256));
+            }
+            cred[0] = 0;
+            cgreen[0] = 0;
+            cblue[0] = 0;
+        }
+
+        private int Check(Complex c)
+        {
+            Complex z = new Complex(0, 0);
+            //Complex cst = new Complex(-0.553373439995225f, 0.5028056112224449f);
+            //Complex cst = new Complex(-0.7513694319791928f, 0.12765531062124258f);
+            //-0.6836339610373092f, 0.38817635270541084f
+            for (int i = 0; i < iter; ++i)
+            {
+                if (z.Mod() > 2)
+                {
+                    return i;
+                }
+                z = z.Square() + c;// new Complex(1, 0);
+                //^3  0.7  0.35
+            }
+            return 0;
+        }
+
+        private void Draw()
+        {
+            Console.WriteLine($"{rx1} {rx2}");
+            Graphics graph = this.pictureBox1.CreateGraphics();
+            graph.Clear(Color.Black);
+            for (float x = dx1; x <= dx2; x += 1)
+            {
+                for (float y = dy1; y <= dy2; y += 1)
+                {
+                    float rx = (rx2 - rx1) * x / (dx2 - dx1) + rx1;
+                    float ry = (ry2 - ry1) * y / (dy2 - dy1) + ry1;
+                    Complex z = new Complex(rx, ry);
+                    int res = Check(z);
+                    Brush pen = new SolidBrush(Color.FromArgb(0, res * 255 / iter, 0));
+                    //Console.WriteLine(Check(z));
+                    graph.FillRectangle(pen, x, y, 1, 1);
+                }
+            }
+        }
+    }
+    public class Complex
+    {
+        public float a;
+        public float b;
+
+        public Complex(float new_a, float new_b)
+        {
+            a = new_a;
+            b = new_b;
+        }
+
+        public float Mod()
+        {
+            return (float)Math.Sqrt(a * a + b * b);
+        }
+        public Complex Square()
+        {
+            return new Complex(a * a - b * b, a * b + b * a);
+        }
+
+        public static Complex operator +(Complex x, Complex y)
+        {
+            return new Complex(x.a + y.a, x.b + y.b);
+        }
+        public static Complex operator -(Complex x, Complex y)
+        {
+            return new Complex(x.a - y.a, x.b - y.b);
+        }
+        public static Complex operator *(Complex x, Complex y)
+        {
+            return new Complex(x.a * y.a - x.b * y.b, x.a * y.b + x.b * y.a);
+        }
+    }
+
+}
